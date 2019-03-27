3df0cbc29c882ee420d1bdfcf7caccb965ef07c1
carolinaazevedo <cavacaaz@gmail.com>
2019-03-27 15:16:51 +0000

    "fixed bug in DFS"

--- src/DFS.cpp
+++ src/DFS.cpp
@@ -9,7 +9,7 @@ Graph::Graph(int V){
 
 }
 
-void Graph::Graph(int v, int w){
+void Graph::addEdgeGraph(int v, int w){
 
   adj[v].push_back(w);
 
@@ -29,15 +29,13 @@ void Graph::DFSaux(int v, bool visited[]){
 
 //Uses DFSaux recursivly
 
-void Graph::DFS(){
+void Graph::DFS(int v){
 
     //Mark all the vertices as not visited
     bool *visited = new bool[V];
-    for(int i = 0; i < V; i++)
-        if(visited[i] = false);
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
 
-    //Call the helper function to print DFS starting from all vertices
-    for(int i = 0; i < V; i++)
-        if(visited[i] == false)
-        DFSaux(i, visited);
+    // Call the recursive aux function to print DFS
+            DFSaux(v, visited);
 }
