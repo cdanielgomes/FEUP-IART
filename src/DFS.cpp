8b879ae84485298f8e1316e2a8b1983be3022f3a
carolinaazevedo <cavacaaz@gmail.com>
2019-04-02 00:17:31 +0100

    DFS

--- src/DFS.cpp
+++ src/DFS.cpp
@@ -3,59 +3,50 @@
 
 using namespace std;
 
-int maxplays = 5;
+Graph::Graph(Node n){
 
-Graph::Graph(int V){
-
-	this->V = V;
-	adj = new list<int>[V];
-
-}
-
-void Graph::addEdge(int v, int w){
-
-  adj[v].push_back(w);
+	Node * n = new Node();
+	adj = n.getState().getAdjacents();
 
 }
 
 
-void Graph::DFSaux(int v, bool visited[], int cost){
+
+void Graph::DFSaux(Node n, bool visited[], int pathCost){
 
 	if(cost > maxplays){
 		return;
 	}
 
-  visited[v] = true; //Mark the current node as visited
-  cout << v << " ";
+  visited[n] = true; //Mark the current node as visited
+  cout << n << " ";
 
   //Recur for all the vertices adjacent to this vertex
 	//To link to game -> subst adj by all possible plays
-  list<int>::iterator i;
-  for(i = adj[v].begin(); i!= adj[v].end(); i++)
+  pair<int, int>::iterator i;
+  for(i = adj.begin(); i!= adj.end(); i++)
       if(!visited[*i])
-        DFSaux(*i, visited, cost+1);
+        DFSaux(*i, visited, pathCost+1);
 }
 
 //Uses DFSaux recursivly
 
-void Graph::DFS(int v){
+void Graph::DFS(Node n){
 
 
     //Mark all the vertices as not visited
-    bool *visited = new bool[V];
-        for (int i = 0; i < V; i++)
+    bool *visited = new bool[n];
+        for (int i = 0; i < n; i++)
             visited[i] = false;
 
     // Call the recursive aux function to print DFS
-            DFSaux(v, visited, 1);
+            DFSaux(n, visited, 1);
 }
 
 
-void Graph::DFSIterative(int v){
-	maxplays = 1;
+void Graph::DFSIterative(Node n){
 
-	while(maxplays < 20){ //change to isGoalState if is finish condition
-		DFS(v);
-		maxplays++;
+	while(!isGoalState(n)){
+		DFS(n);
 	}
 }
--- src/DFS.h
+++ src/DFS.h
@@ -3,6 +3,7 @@
 
 #include "search.h"
 #include "state.h"
+#include "node.h"
 #include <iostream>
 #include <list>
 
@@ -10,17 +11,21 @@ class Graph {
 
 private:
 
-    int V; // No. of vertices
-    std::list<int> *adj; // Pointer to an array containing adjacency lists
-    void DFSaux(int v, bool visited[],int cost); // A function used by DFS
-
+    //Node n;
+    //int V; // No. of vertices
+    //std::list<int> *adj; // Pointer to an array containing adjacency lists
+    //void DFSaux(int v, bool visited[],int cost); // A function used by DFS
+    void DFSaux(Node n, bool visited[],int pathCost);
 
 public:
 
-    Graph(int v); // Constructor
-    void addEdge(int v, int w); // function to add an edge to graph
-    void DFS(int v); // prints DFS of the complete graph
-    void DFSIterative(int v);
+    //Graph(int v); // Constructor
+    Graph(Node n);
+    //void addEdge(int v, int w); // function to add an edge to graph
+    void DFS(Node n);
+    //void DFS(int v); // prints DFS of the complete graph
+    void DFSIterative(Node n);
+    //void DFSIterative(int v);
 
 };
 
