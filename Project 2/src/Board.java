a9201c206e315289f1e54e420993008c5cc096a7
joanasmramos <joanasmramos98@gmail.com>
2019-05-03 11:17:21 +0100

    advances on empty hole

--- Project 2/src/Board.java
+++ Project 2/src/Board.java
@@ -24,9 +24,52 @@ class Board {
         }
     }
 
-    // player = 1 OR player = 2
-    void emptyHole(int player, int holeNumber) {
+    private boolean removeBean(int row, int column) {
+        // if there are beans in the hole
+        if(board[row][column] > 0) {
+            board[row][column]--;
+            return true;
+        }
 
+        return false;
+    }
+
+    private void addBean(int row, int column) {
+        board[row][column]++;
+    }
+
+    // player = 1 OR player = 2
+    boolean emptyHole(int player, int holeNumber) {
+        int row = player-1, column = ((player == 1) ? holeNumber : holeNumber - 1);
+        int numberOfBeans = board[row][column];
+
+        if(numberOfBeans == 0) {
+            System.out.println("You have to choose a non-empty hole!");
+            return false;
+        }
+
+        Integer finishedRow = null, finishedColumn = null;
+        int i=row, j=((row==0) ? column-1 : column+1);
+        while(numberOfBeans > 0) {
+            if(j<COLUMNS && j >= 0) {
+                if(removeBean(row, column)) {
+                    numberOfBeans--;
+                    addBean(i,j);
+                    j=((i==0) ? j - 1 : j + 1);
+                    finishedRow=i; finishedColumn=j;
+                }
+            }
+            else {
+                if(i==0) {
+                    i=1; j=0;
+                }
+                else {
+                    i=0; j=COLUMNS-1;
+                }
+            }
+        }
+
+        return true;
     }
 
     int getPot1() {
@@ -59,6 +102,8 @@ class Board {
             }
 
         }
+
+        System.out.println();
     }
 
 }
